65e58171fbd22b9052757e151284b779d4c045ff
Dominik Ringler <dom.01@icloud.com>
2016-01-09 22:46:28 +0100

    Initial Commit

---
+++ CustomScrollViewTests/CustomScrollViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  CustomScrollViewTests.swift
+//  CustomScrollViewTests
+//
+//  Created by Dominik on 09/01/2016.
+//  Copyright © 2016 Dominik. All rights reserved.
+//
+
+import XCTest
+@testable import CustomScrollView
+
+class CustomScrollViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
