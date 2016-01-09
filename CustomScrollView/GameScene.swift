97f30fe1a288a637fb512b82de70389f3f149022
Dominik Ringler <dom.01@icloud.com>
2016-01-09 23:23:05 +0100

    v1.0

--- CustomScrollView/GameScene.swift
+++ CustomScrollView/GameScene.swift
@@ -10,23 +10,30 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    let moveableNode = SKNode()
+    /// Scroll view
     var scrollView: CustomScrollView!
+    
+    /// Moveable node in the scrollView
+    let moveableNode = SKNode()
     
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
+        /// add moveable node
         addChild(moveableNode)
         
+        /// set up scrollView
         scrollView = CustomScrollView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height), scene: self, moveableNode: moveableNode)
-        scrollView.contentSize = CGSizeMake(self.frame.size.width, self.frame.size.height * 2)
+        scrollView.contentSize = CGSizeMake(self.frame.size.width, self.frame.size.height * 2) // * 2 makes it twice as long as screen
         view.addSubview(scrollView)
         
+        /// sprite
         let sprite = SKSpriteNode(color: SKColor.redColor(), size: CGSize(width: 50, height: 44))
         sprite.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) - self.frame.size.height)
         moveableNode.addChild(sprite)
         
+        /// label
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "Hello, World!"
         myLabel.fontSize = 45
